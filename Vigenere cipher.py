6d919a57f6bd989b6fb52f4446403d912b6640c5
rayrayyrayyy <allssndr.ray@gmail.com>
2023-04-21 00:37:16 +0800

    dictionary

--- Vigenere cipher.py
+++ Vigenere cipher.py
@@ -1,4 +1,9 @@
 # create a dictionary
+alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+letter_length = dict(zip(alphabet, range(len(alphabet))))
+index_lenght = dict(zip(range(len(alphabet)), alphabet))
+
 # define encryption
   # split the message to match the lenght of the key
   # convert the message to index and add the key
