eed764b9bdb0db6d971ad5b72a2168d57746be80
rayrayyrayyy <allssndr.ray@gmail.com>
2023-04-21 00:08:55 +0800

    pseudocode

--- Vigenere cipher.py
+++ Vigenere cipher.py
@@ -1,1 +1,7 @@
-hello
+# create a dictionary
+# define encryption
+  # split the message to match the lenght of the key
+  # convert the message to index and add the key
+# define decryption
+    # split the cipher to match the lenght of the key
+    # convert the message to index and add the key
